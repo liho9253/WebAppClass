810bdce47ce385315d6a0137c2ae9a7c81fff57d
liho9253 <timmy89566@gmail.com>
2021-12-10 11:33:21 +0800

    20211210

---
+++ WebApp_F/w_20211210.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApp_F{
+    public partial class w_20211210 : System.Web.UI.Page{
+        protected void Page_Load(object sender, EventArgs e){
+            //Data Source=(localdb)\MSSQLLocalDB;
+            //Initial Catalog=Test;Integrated Security=True;
+            //Connect Timeout=30;Encrypt=False;
+            //TrustServerCertificate=False;
+            //ApplicationIntent=ReadWrite;
+            //MultiSubnetFailover=False;
+            //User ID = sa;Password = 12345;
+
+            string s_ConnStr = "Data Source=(localdb)\\MSSQLLocalDB;" +
+                                "Initial Catalog = Test;" +
+                                "Integrated Security = True;" +
+                                "Connect Timeout = 30;"+
+                                "Encrypt = False;" +
+                                "TrustServerCertificate=False;" +
+                                "ApplicationIntent =ReadWrite;" +
+                                "MultiSubnetFailover =False;" +
+                                "User ID = sa;Password = 12345;";
+            
+            try {
+                SqlConnection o_SQLcon = new SqlConnection(s_ConnStr);
+
+                SqlCommand o_Com = new SqlCommand("Select * from Users", o_SQLcon);
+                o_SQLcon.Open();
+                SqlDataReader o_R = o_Com.ExecuteReader();
+                for (; o_R.Read();) {
+                    for (int i = 0; i < o_R.FieldCount; i++) {
+                        Response.Write("&nbsp;&nbsp;"+o_R[i].ToString());
+                    }
+                    Response.Write("<br/>");
+                }
+                o_SQLcon.Close();
+            } catch (Exception ex) {
+                Response.Write(ex.ToString());
+            }
+            
+        }
+    }
+}
---
+++ WebApp_F/w_20211210_2.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace WebApp_F{
+    public partial class w_20211210_2 : System.Web.UI.Page{
+        protected void Page_Load(object sender, EventArgs e){
+            string s_ConnStr = "Data Source=(localdb)\\MSSQLLocalDB;" +
+                                "Initial Catalog = Test;" +
+                                "Integrated Security = True;" +
+                                "Connect Timeout = 30;"+
+                                "Encrypt = False;" +
+                                "TrustServerCertificate=False;" +
+                                "ApplicationIntent =ReadWrite;" +
+                                "MultiSubnetFailover =False;" +
+                                "User ID = sa;Password = 12345;";
+
+            try {
+                SqlConnection o_Conn = new SqlConnection(s_ConnStr);
+                SqlDataAdapter o_A = new SqlDataAdapter("Select * from Users", o_Conn);
+                DataSet o_d = new DataSet();
+                o_A.Fill(o_d, "ds_Res");
+                gv_DataShow.DataSource = o_d;
+                gv_DataShow.DataBind();
+                o_Conn.Close();
+            } catch (Exception ex) {
+                Response.Write(ex.ToString());
+            }
+        }
+    }
+}
